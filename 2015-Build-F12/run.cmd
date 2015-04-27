2b6af786e827a919cfbfa866965140ea38ba3a64
AndySterland <andster@microsoft.com>
2015-04-26 20:46:01 -0700

    Updating run.cmd to be relative.

--- 2015-Build-F12/run.cmd
+++ 2015-Build-F12/run.cmd
@@ -1,2 +1,1 @@
-"C:\Program Files (x86)\Internet Explorer\iexplore.exe" http://localhost:9123/2015BuildF12Demo.html
-"C:\Program Files (x86)\IIS Express\iisexpress" /path:C:\src\Talks\2015-Build-F12 /port:9123  
+"C:\Program Files (x86)\IIS Express\iisexpress" /path:%~dp0  /port:9123  
